34231a540798e0ff75114eaa00385e611a881657
Rajeshwarikoli31 <rajeshwarikoli08@gmail.com>
2021-06-05 15:49:14 +0530

    mantra

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
+  collection.
   client.close();
 });
